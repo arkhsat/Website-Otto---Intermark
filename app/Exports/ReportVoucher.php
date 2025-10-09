072f6324eae622c93bb8c416e7d3520150f3a57f
Zhandy <zhandy@nusantarainfrastructure.com>
2025-10-09 18:35:55 +0700

    Testing Index Report Voucher Excel

--- app/Exports/ReportVoucher.php
+++ app/Exports/ReportVoucher.php
@@ -2,15 +2,33 @@
 
 namespace App\Exports;
 
-use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\Exportable;
+use Maatwebsite\Excel\Concerns\WithMultipleSheets;
 
-class ReportVoucher implements FromCollection
+class ReportVoucher implements WithMultipleSheets
 {
-    /**
-    * @return \Illuminate\Support\Collection
-    */
-    public function collection()
+    use Exportable;
+
+    protected $results;
+    protected $startDate;
+    protected $endDate;
+    protected $judul;
+
+    public function __construct($results, $startDate, $endDate, $judul)
     {
-        //
+        $this->results = $results;
+        $this->startDate = $startDate;
+        $this->endDate = $endDate;
+        $this->judul = $judul;
+    }
+
+    public function sheets(): array
+    {
+        return [
+            new ReportQtyExport1($this->results, $this->startDate, $this->endDate, 'Report Penggunaan Voucher'),
+            new ReportQtyExport2($this->results, $this->startDate, $this->endDate, 'Transaksi Perjam Mobil'),
+            new ReportQtyExport3($this->results, $this->startDate, $this->endDate, 'Transaksi Perjam Motor'),
+            new ReportQtyExport4($this->results, $this->startDate, $this->endDate, 'Transaksi Perjam Masuk Mobil'),
+        ];
     }
 }
