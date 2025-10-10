8979b4d790ced442e6548ddfb38a87723561ebda
Zhandy <zhandy@nusantarainfrastructure.com>
2025-10-10 14:13:08 +0700

    Penambahan App Location Name di Aplikasi

--- config/app.php
+++ config/app.php
@@ -16,6 +16,7 @@ return [
     */
 
     'name' => env('APP_NAME', 'Laravel'),
+    'location' => env('APP_LOCATION', 'Intermark'),
 
     /*
     |--------------------------------------------------------------------------
